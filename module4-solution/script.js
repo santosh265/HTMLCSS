e8bebbddc2e4300fd9900f947e0061e2d6d9eddc
Santosh Arvapally <santosh_arvapally@epam.com>
2020-06-25 21:01:50 +0530

    HEy

---
+++ module4-solution/script.js
@@ -0,0 +1,8 @@
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+for (name in names) {
+  if (names[name].charAt(0)=="j"||names[name].charAt(0)=="J") {
+     byeSpeaker.speak(names[name]);
+  } else {
+    helloSpeaker.speak(names[name]);
+  }
+}
